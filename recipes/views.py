cf341f38fe32bf6626435862c893d12e5720c41e
Victor Contreras <contreras3991@gmail.com>
2024-10-09 13:15:02 -0300

    commit

--- recipes/views.py
+++ recipes/views.py
@@ -7,4 +7,4 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/recipe-view.html')
